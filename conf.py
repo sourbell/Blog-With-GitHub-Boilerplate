331f09fed16ce88022b812f54ce88e4467c4dcb1
Bristletail <i@sourbell.im>
2019-12-24 23:54:27 +0800

    update

--- conf.py
+++ conf.py
@@ -3,24 +3,24 @@
 """
 
 # For Maverick
-site_prefix = "https://project.sourbell.im/"
+site_prefix = "/"
 source_dir = "../src/"
 build_dir = "../dist/"
 index_page_size = 10
 archives_page_size = 20
 enable_jsdelivr = {
-    "enabled": False,
+    "enabled": True,
     "repo": ""
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "豪猪尾巴"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
 author = "Bristletail"
 email = "i@sourbell.im"
 author_homepage = "https://sourbell.im"
-description = "只坚持一种正义。我的正义。"
+description = "Alive"
 key_words = ['Maverick', 'sourbell', 'Bristletail', 'blog']
 language = 'zh-CN'
 external_links = [
--- src/about.md
+++ src/about.md
@@ -4,19 +4,12 @@ title: 关于
 slug: about
 date: 2017/06/28 17:26:00
 status: publish
-author: AlanDecode
-categories: 
+author: Bristletail
+categories:
   - Maverick
   - Dev
-tags: 
+tags:
   - theme
 ---
 
 这里是我的个人博客，通过 GitHub 管理与构建。
-
-
-## Contact
-
-Email: hi#imalan.cn (replace # with @)
-
-Telegram：[AlanDecode](https://t.me/alandecode)
